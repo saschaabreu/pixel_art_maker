bcf1d804ab7ed75caa9ef1c32ced7818cefb0d60
Sascha Abreu <65729261+saschaabreu@users.noreply.github.com>
2020-05-26 23:13:07 +0200

    make site interactive with javascript

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+// make the event listener for form submit
+document.getElementById("sizePicker").onsubmit = function () {
+    makeGrid();
+}
 
 function makeGrid() {
+    event.preventDefault();
+    // get height and width from html
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+    // get and reset table
+    var table = document.getElementById("pixelCanvas");
+    table.innerHTML = "";
 
-// Your code goes here!
-
+    // nested for loop for making the grid
+    for (let i = 0; i < height; i++) {
+        //insert a row
+        // https://www.w3schools.com/jsref/met_table_insertrow.asp
+        let row = table.insertRow(i);
+        for (let j = 0; j < width; j++) {
+            // for each column, insert a cell
+            let cell = row.insertCell(j);
+            // add the event listener to the cell to change color
+            cell.addEventListener("click", function(e) {
+                // get the color
+                let color = document.getElementById('colorPicker').value;
+                // set the color on the cell as background color
+                event.target.style.backgroundColor = color;
+            });
+        }
+    }
 }
